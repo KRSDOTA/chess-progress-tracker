3a8c8479bbfb81ac3a1fe8f45c9ecdd01ac3fdb1
KRSProg <meepo0nly2@gmail.com>
2023-12-31 11:25:54 +0000

    Added Javadoc to method implementation

--- src/main/java/chess/progress/tracker/chessprogresstracker/ratingchange/RatingChangeService.java
+++ src/main/java/chess/progress/tracker/chessprogresstracker/ratingchange/RatingChangeService.java
@@ -9,11 +9,10 @@ import java.util.Set;
 public interface RatingChangeService {
 
     /**
-     *
-     * @param username
-     * @param start
-     * @param end
-     * @return
+     * Computes a set of rating changes, each element being a rating change object, one for each discipline
+     * @param username username to get ratings for
+     * @param start closed interval lower bound
+     * @param end closed interval upper bound
      */
     Set<RatingChange> getCrossDisciplineChangesForInterval(String username, Instant start, Instant end);
 
