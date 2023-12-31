5aba9a3a5e3f4a40a1f3fd497614b9ffe5a5b530
KRSProg <meepo0nly2@gmail.com>
2023-12-31 09:52:16 +0000

    Added in test to make sure sorting is done correctly

--- src/test/java/matcharchive/ChessPubApiMatchServiceTest.java
+++ src/test/java/matcharchive/ChessPubApiMatchServiceTest.java
@@ -11,9 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Year;
+import java.time.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -77,9 +76,28 @@ public class ChessPubApiMatchServiceTest {
         assertThat(matchList).hasSize(2);
     }
 
+    @Test
+    void shouldSuccessfullySortInDescendingOrder() {
+        final String username = "something";
+        final LocalDate localDate = LocalDate.of(2000, 1, 12);
+        final String fakeUrl = "/player/" + username + "/games/" + localDate.getYear() + "/" + localDate.getMonthValue();
+        when(matchEndpointUrlBuilder.buildGamesUrl(eq(username), any())).thenReturn(fakeUrl);
+        returnFakeGamesWithUrlCall(fakeUrl);
+
+        final List<Match> matchList = chessPubApiMatchArchiveService.getAllMatches(username, localDate);
+
+        assertThat(matchList.get(0).compareTo(matchList.get(1))).isEqualTo(-1);
+    }
+
+
     private void returnFakeGamesWithUrlCall(String fakeUrl){
         Games fakeGames = new Games();
-        fakeGames.setGames(List.of(new Match(), new Match()));
+        final Match match1 = new Match();
+        match1.setEnd_time(Instant.ofEpochSecond(LocalDate.of(2000, 10, 12).toEpochSecond(LocalTime.now(), ZoneOffset.UTC)));
+        final Match match2 = new Match();
+        match2.setEnd_time(Instant.now());
+        final ArrayList<Match> games = new ArrayList<>(List.of(match1, match2));
+        fakeGames.setGames(games);
         when(restTemplate.getForObject(fakeUrl, Games.class)).thenReturn(fakeGames);
     }
 
